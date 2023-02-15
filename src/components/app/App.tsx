02a2991aea4709a260677d506cb8046429c5cf13
Sara Carreras <saracc621@gmail.com>
2023-02-15 13:28:14 +0100

    app structure with title and sidebar component

---
+++ src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Sidebar } from '../sidebar/sidebar';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Working Calendar 2021</h1>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
