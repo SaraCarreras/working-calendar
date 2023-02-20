5951eb3a44e23e5663652460c319f980d921da4c
Sara Carreras <saracc621@gmail.com>
2023-02-20 17:37:11 +0100

    showing holidays fetched

--- src/components/holidayList/holidayList.tsx
+++ src/components/holidayList/holidayList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { HolidayListProps } from '../../interfaces/iHolidays';
 import holidayList from './holidayList.module.scss';
 
-export function HolidayList() {
-  return <>SOY HOLI</>;
+export function HolidayList({ holidaysList }: HolidayListProps) {
+  console.log(holidaysList);
+  return (
+    <div className={holidayList.container}>
+      <h3>Days marked as holidays:</h3>
+      <ol>
+        {holidaysList.map((holiday) => (
+          // console.log(holiday.date.getDay()),
+          <li key={holiday.day}>
+            <span>{holiday.description}</span>
+            <span> Día: {holiday.day}</span>
+            {/* <span> Mes: {holiday.date.getMonth() + 1}</span> */}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
