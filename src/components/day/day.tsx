a364cd98157892b78755f14638e33e487442b167
Sara Carreras <saracc621@gmail.com>
2023-02-20 21:57:08 +0100

    logic to show festive days on calendar

--- src/components/day/day.tsx
+++ src/components/day/day.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { DayProps } from '../../interfaces/calendarBodyProps';
+import { DayProps } from '../../interfaces/typeProps';
 import day from './day.module.scss';
 
-export function Day({ date, isCurrentMonth }: DayProps) {
+export function Day({ date, isCurrentMonth, holidaysList }: DayProps) {
   //conditional compares actual date with date shown on calendar
   const isToday =
     date.getFullYear() === new Date().getFullYear() &&
     date.getMonth() === new Date().getMonth() &&
     date.getDate() === new Date().getDate();
-  //console.log(new Date().getDate());
+
+  //I need to compare actual date with shown date but since I'm using data from 2021 transformed
+  //I need to transform it again substracting 2 years to the comparisson date to get true
+  const isoDate = date.toISOString();
+  const year = parseInt(isoDate.split('-')[0]);
+  const newDate = new Date(date);
+  newDate.setFullYear(year - 2);
+  const newIsoDate = newDate.toISOString().split('T')[0];
+
+  const holiday = holidaysList.find((holiday) => holiday.day === newIsoDate);
+
+  const typeClass = holiday ? day[holiday.type] : '';
 
   return (
     <div className={day.dayContainer}>
       <div
         className={`${day.day} ${isCurrentMonth ? '' : day.disabledDay} ${
           isToday ? day.today : ''
-        }`}
+        } ${typeClass}`}
       >
         {date.getDate()}
       </div>
