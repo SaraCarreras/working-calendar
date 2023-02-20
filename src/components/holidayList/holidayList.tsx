6348bdb076b65a7b91cf282ef639c931b5eecedc
Sara Carreras <saracc621@gmail.com>
2023-02-20 21:56:50 +0100

    conditional classname to show festives

--- src/components/holidayList/holidayList.tsx
+++ src/components/holidayList/holidayList.tsx
@@ -26,9 +26,17 @@ export function HolidayList({ holidaysList }: HolidayListProps) {
 
           const monthName = meses[fecha.getMonth()];
           const dayNumber = fecha.getDate();
+          let holidayTypeClass = '';
+          if (holiday.type === 'national') {
+            holidayTypeClass = holidayList.national;
+          } else if (holiday.type === 'regional') {
+            holidayTypeClass = holidayList.regional;
+          } else if (holiday.type === 'local') {
+            holidayTypeClass = holidayList.local;
+          }
 
           return (
-            <li key={holiday.day}>
+            <li key={holiday.day} className={holidayTypeClass}>
               <p className={holidayList.day}>
                 {`${dayNumber} de ${monthName}-`}{' '}
                 <span className={holidayList.description}>
