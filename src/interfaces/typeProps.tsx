8c2b77bd7efe44f3b33b972d143a0cab82193df8
Sara Carreras <saracc621@gmail.com>
2023-02-20 21:55:45 +0100

    changed name's file

--- src/interfaces/typeProps.tsx
+++ src/interfaces/typeProps.tsx
@@ -1,8 +1,12 @@
+import { iHolidays } from './iHolidays';
+
 export type CalendarBodyProps = {
   date: Date;
+  holidaysList: iHolidays[];
 };
 
 export type DayProps = {
   date: Date;
   isCurrentMonth: boolean;
+  holidaysList: iHolidays[];
 };
