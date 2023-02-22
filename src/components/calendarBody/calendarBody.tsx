106d38db4b2aafe610852d2dd5fbf1b73cf034cf
Sara Carreras <saracc621@gmail.com>
2023-02-22 15:24:12 +0100

    changing comments

--- src/components/calendarBody/calendarBody.tsx
+++ src/components/calendarBody/calendarBody.tsx
@@ -6,21 +6,22 @@ import { Day } from '../day/day';
 export function CalendarBody({ date, holidaysList }: CalendarBodyProps) {
   const weekDays = ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sab', 'Dom'];
 
-  //creating emptyCells to disabled days
-  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-
+  //calculating emptycells at begining month
   //it needs to be -1 bcs first day of week is Sun = 0
+  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
   const firstDayOfWeek = firstDayOfMonth.getDay() - 1;
-  console.log(firstDayOfWeek);
-  const emptyCells = Array.from({ length: firstDayOfWeek }, (_, i) => i);
 
+  //calculating emptyCells at the end of month
+  const emptyCells = Array.from({ length: firstDayOfWeek }, (_, i) => i);
+  console.log(emptyCells);
+
+  //calculating number days in a Month by getting the last the of actual month
   const daysInMonth = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
   ).getDate();
 
-  //creating arr of days
   const days = Array.from(
     { length: daysInMonth },
     (_, i) => new Date(date.getFullYear(), date.getMonth(), i + 1)
@@ -34,28 +35,35 @@ export function CalendarBody({ date, holidaysList }: CalendarBodyProps) {
         ))}
       </ul>
       <div className={calendarBody.daysContainer}>
-        {[
-          ...Array(Math.ceil((emptyCells.length + daysInMonth) / 7)).keys(),
-        ].map((rowIndex) => (
-          <div key={rowIndex} className={calendarBody.row}>
-            {[...Array(7).keys()].map((colIndex) => {
-              const dayIndex = rowIndex * 7 + colIndex - emptyCells.length;
-              const day = days[dayIndex];
-              return day ? (
-                <Day
-                  key={day.toISOString()}
-                  date={day}
-                  holidaysList={holidaysList}
-                />
-              ) : (
-                <div
-                  key={`empty-cell-${colIndex}`}
-                  className={calendarBody.emptyCell}
-                />
-              );
-            })}
-          </div>
-        ))}
+        {
+          //creating row's calendar
+          [
+            ...Array(Math.ceil((emptyCells.length + daysInMonth) / 7)).keys(),
+          ].map((rowIndex) => (
+            <div key={rowIndex} className={calendarBody.row}>
+              {
+                //creating cells on each row
+                [...Array(7).keys()].map((colIndex) => {
+                  //creating de number of the day to show on Day element
+                  const dayIndex = rowIndex * 7 + colIndex - emptyCells.length;
+                  const day = days[dayIndex];
+                  return day ? (
+                    <Day
+                      key={day.toISOString()}
+                      date={day}
+                      holidaysList={holidaysList}
+                    />
+                  ) : (
+                    <div
+                      key={`empty-cell-${colIndex}`}
+                      className={calendarBody.emptyCell}
+                    />
+                  );
+                })
+              }
+            </div>
+          ))
+        }
       </div>
     </div>
   );
